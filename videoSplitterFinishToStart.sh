168f31b24314ea6f0c6eccfb625e2b7b5056374c
hiremepls <dpitonak@seznam.cz>
2026-03-14 08:45:56 +0100

    Create videoSplitterFinishToStart.sh

---
+++ videoSplitterFinishToStart.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# This script will split the video from the finish to start into p parts of i minute intervals
+# 35 minute video with parameters 10 2 will output 2 videos, last 10 miuntes, and last 20 minutes
+# Usage: ./script.sh input.mp4 i p
+# i = interval length in minutes
+# p = number of parts
+# Example: ./script.sh video.mp4 5 3
+
+input="$1"
+i="$2"
+p="$3"
+filename="${input%.*}"  # Remove extension
+
+duration=$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "$input")
+duration_sec=$(echo "$duration" | awk '{print int($1)}')
+
+for (( n=1; n<=$p; n++ )); do
+    length=$((i * n * 60))
+    if [ $length -gt $duration_sec ]; then
+        length=$duration_sec
+    fi
+    output="${filename}_output_${i}min_${n}of${p}.mp4"
+    ffmpeg -i "$input" -ss $(($duration_sec - $length)) -c copy "$output"
+    echo "Created $output (last $((length/60)) minutes)"
+done
